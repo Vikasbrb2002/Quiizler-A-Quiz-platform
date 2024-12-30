2f8e884b8f2ed47429f18bab1a7cefbd25e7cb40
Vikas <vikasbrb2002@gmail.com>
2024-12-30 19:14:43 +0530

    login page added

--- frontend/src/app/Signup/page.jsx
+++ frontend/src/app/Signup/page.jsx
@@ -7,7 +7,7 @@ import { cn } from "@/utils/cn";
 import {
   IconBrandGithub,
   IconBrandGoogle,
-  IconBrandOnlyfans,
+  
 } from "@tabler/icons-react";
 
 export default function Signup() {
@@ -19,7 +19,7 @@ export default function Signup() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Quiizler
+        Sign Up to Quiizler
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
         Login to Quiizler if you can because we don&apos;t have a login flow
@@ -29,29 +29,36 @@ export default function Signup() {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            <Input id="firstname" placeholder="vikas" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Input id="lastname" placeholder="kumar" type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
         </LabelInputContainer>
+
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
           <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your Twitter Password</Label>
+
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Re-enter Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+
+        {/* <LabelInputContainer className="mb-8"> */}
+          {/* <Label htmlFor="twitterpassword">Your Twitter Password</Label>
           <Input
             id="twitterpassword"
             placeholder="••••••••"
             type="password" // Fixed invalid type
           />
-        </LabelInputContainer>
+        </LabelInputContainer> */}
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -66,7 +73,7 @@ export default function Signup() {
         <div className="flex flex-col space-y-4">
           <SocialButton icon={<IconBrandGithub />} label="GitHub" />
           <SocialButton icon={<IconBrandGoogle />} label="Google" />
-          <SocialButton icon={<IconBrandOnlyfans />} label="OnlyFans" />
+          {/* <SocialButton icon={<IconBrandOnlyfans />} label="OnlyFans" /> */}
         </div>
       </form>
     </div>
